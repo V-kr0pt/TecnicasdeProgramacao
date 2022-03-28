51b5ff94a1c0d03849dd51647b87462eda7f5370
Vitor França <vitor.franca@cear.ufpb.br>
2022-03-28 10:36:23 -0300

    org em pastas

---
+++ aula03/exercicios_aula03.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+//Para a questao7
+#define pi 3.14159
+
+void questao1(void);
+void questao2(void);
+void questao3(void);
+void questao4(void);
+void questao5(void);
+void questao6(void);
+void questao7(void);
+// void questao8(void);
+void questao9(void);
+void questao10(void);
+// void piramide(void);
+
+
+int main(){
+    // questao1();
+    // questao2();
+    // questao3();
+    // questao4();
+    // questao5();
+    // questao6();
+    // questao7();
+    // questao8();
+    // questao9();
+    // piramide();
+}
+
+void questao1(){
+    /* 
+    Imprima (mostre na tela) seu nome completo 
+    e sua idade em linhas separadas.
+    */
+    std::cout << "Vitor de Sousa França" << "\n";
+    std::cout << "22" << "\n";
+}
+
+void questao2(){
+    /*
+    Imprima a raiz quadrada dos seguinte números:
+     4, 19 e 16 
+    */
+   std::cout << sqrt(4) << "\n" << sqrt(19) << "\n" 
+                                << sqrt(16) << "\n";
+
+}
+
+void questao3(){
+    /*
+    Imprima a raiz quadrada de um 
+    valor inteiro digitado pelo usuário.
+    */
+    int x;
+    std::cout << "Digite um valor: ";
+    std::cin >> x;
+    std::cout << "A raiz quadrada do valor: "<< sqrt(x) 
+                                             << "\n";
+}
+
+void questao4(){
+    /*
+    Imprima a raiz quadrada de um 
+    valor float digitado pelo usuário.
+    */questao6();
+    float x;
+    std::cout << "Digite um valor: ";
+    std::cin >> x;
+    std::cout << "A raiz quadrada do valor: "<< sqrt(x) 
+                                             << "\n";
+}
+
+void questao5(){
+    /*
+    Imprima a hipotenusa de um triângulo retângulo 
+    cujos catetos sejam digitados pelo usuário.
+    */
+
+    float x, y, h;
+    std::cout << "Digite o tamanho de um cateto: ";
+    std::cin >> x;
+    std::cout << "Digite o tamanho do outro cateto: ";
+    std::cin >> y;
+    h = sqrt(pow(x,2) + pow(y,2));
+    std::cout << "O tamanho da hipotenusa do triangulo retangulo: " 
+                                                    << h << "\n";
+}
+
+void questao6(){
+    /*
+    Imprima o seno, cosseno e tangente dos ângulos 
+    (exceto o reto) de um triângulo retângulo cujos 
+    catetos sejam digitados pelo usuário
+    */
+    float theta, alpha;
+    std::cout << "Digite o ângulo theta (°): " ;
+    std::cin >> theta;
+    std::cout << "Digite o ângulo alpha (°): ";
+    std::cin >> alpha;
+    if(theta == 90 || alpha == 90){
+        std::cout << "O ângulo não pode ser reto.\n" << 
+        "Digite outro valor \n";
+        questao6();        
+    }else{
+        theta *= M_PI/180;
+
+        std::cout << "\nseno theta = " << sin(theta) << "\n";
+        std::cout << "seno alpha = " << sin(alpha) << "\n";
+        std::cout << "cosseno theta = " << cos(theta) << "\n";
+        std::cout << "cosseno alpha = " << cos(alpha) << "\n";
+        std::cout << "tangente theta = " << tan(theta) << "\n";
+        std::cout << "tangente alpha = " << tan(alpha) << "\n";
+    }
+}
+
+void questao7(){
+    /* 
+    Imprima o raio, o perímetro e a área de 
+    um círculo cujo raio seja dado pelo usuário. 
+    Utilize o operador #define para definir o valor de pi.
+    */
+   int r;
+
+   std::cout << "Digite o raio do circulo: ";
+   std::cin >> r;
+   std::cout << "O perímetro do circulo: p = " << 2*pi*r << "\n";
+   std::cout << "A área do circulo: A = " << pi*pow(r,2) << "\n";
+ 
+}
+
+// void questao8(){} FALTANDO SLIDE
+
+
+namespace VITOR{
+    int i=1, j=2;
+    float z = 6.28;
+}
+
+
+void questao9(){
+    std::cout << "i=" << VITOR::i << "\n" << 
+    "sen("<< VITOR::i << ") = " << sin(VITOR::i) << "\n"
+    "cos("<< VITOR::i << ") = " << cos(VITOR::i) << "\n\n";
+
+    std::cout << "j=" << VITOR::j << "\n" << 
+    "sen("<< VITOR::j << ") = " << sin(VITOR::j) << "\n"
+    "cos("<< VITOR::j << ") = " << cos(VITOR::j) << "\n\n";
+
+    std::cout << "z=" << VITOR::z << "\n" << 
+    "sen("<< VITOR::z << ") = " << sin(VITOR::z) << "\n"
+    "cos("<< VITOR::z << ") = " << cos(VITOR::z) << "\n\n";
+}    
+
+
+/*questão 10
+void piramide(){
+    int width = 113, qntd_estrelas;
+    // int espacos = width - qntd_estrelas;
+    string frase_estrela="", str_espaco;
+
+    //percorrendo as linhas
+    for(int l; l<6; l++){
+        str_espaco = ""; //resetando variável
+
+        for(int j; j<(width-qntd_estrelas)/2; j++){
+            str_espaco += " ";
+        }
+
+        //qntd_estrelas = l*2;
+
+        //for(int k; k<qntd_estrelas; k++){
+        //    frase_estrela+="*";
+        //}
+
+        
+
+    }
+
+}*/
