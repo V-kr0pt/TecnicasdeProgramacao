e01f194d5ed43ab6323d2b44441431988a021c64
kr0pt <vitor.franca@cear.ufpb.br>
2022-04-25 23:42:52 -0300

    media2,media3 funcionando

--- atvidades_pontuadas/atividade.cpp
+++ atvidades_pontuadas/atividade.cpp
@@ -4,54 +4,52 @@ using namespace std;
 
 float media(float n1, float n2);
 float media(float n1, float n2, float n3);
-float media(float n1, float n2, float n3, float n4);
+float media(float N[4]);
 
 int main(){
     int cargahoraria, op;
-    float N1, N2, N3, N4, m;
+    float m, N[4]; 
 
     cout << "Digite a carga horaria da disciplina: ";
     cin >> cargahoraria;
 
-    if(cargahoraria<=45){
+    if(cargahoraria <= 45){
+
         cout << "Adicione as duas notas" << endl;
-        cout << "Primeira Nota: ";
-        cin >> N1;
-        cout << "Segunda Nota: ";
-        cin >> N2;        
-        m = media(N1, N2);
+        for(int i=0; i<2; i++){
+            cout << "Nota " << i+1 << ": ";
+            cin >> N[i];
+        }  
+        m = media(N[0], N[1]);
+
     }else{
         cout << "Escolha a quantidade de notas que serao utilizadas, opcoes: 3 ou 4" << endl;
         cin >> op; 
 
         do{
             if(op == 3){
-                cout << "Primeira Nota: ";
-                cin >> N1;
-                cout << "Segunda Nota: ";
-                cin >> N2;
-                cout << "Terceira Nota: ";
-                cin >> N3;
 
-                m = media(N1, N2, N3);
+                for(int i=0; i < op; i++){
+                    cout << "Nota " << i+1 << ": ";
+                    cin >> N[i];
+                }
+                
+                m = media(N[0], N[1], N[2]);
 
             }else if(op == 4){
-                cout << "Primeira Nota: ";
-                cin >> N1;
-                cout << "Segunda Nota: ";
-                cin >> N2;
-                cout << "Terceira Nota: ";
-                cin >> N3;
-                cout << "Quarta Nota: ";
-                cin >> N4;
 
-                m = media(N1, N2, N3, N4);
+                for(int i=0; i < op; i++){
+                    cout << "Nota " << i+1 << ": ";
+                    cin >> N[i];
+                }
+
+                m = media(N);
             }
-        }while(op!=3 || op!=4);
+        }while(op!=3 && op!=4);
 
     }
 
-
+    cout << "A media final: " << m << endl;
 
     return 0;
 }
@@ -64,8 +62,8 @@ float media(float n1, float n2, float n3){
     return (n1+n2+n3)/3;
 }
 
-float media(float n1, float n2, float n3, float n4){
+float media(float n[4]){
     //melhor fazer um vetor
-    return (n1+n2+n3)/3;
+    // tenho q fazer um sort crescente para usar media3
+    return media(n[0],n[1],n[2]);
 }
-
