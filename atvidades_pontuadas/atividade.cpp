648721b658934d30575bc7e9a10e5e5d5323c71d
kr0pt <vitor.franca@cear.ufpb.br>
2022-04-25 23:58:39 -0300

    adicao de conceitos e correcao do op

--- atvidades_pontuadas/atividade.cpp
+++ atvidades_pontuadas/atividade.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//sobrecarga da funcao media
 float media(float n1, float n2);
 float media(float n1, float n2, float n3);
 float media(float N[4]);
@@ -20,20 +21,21 @@ int main(){
             cout << "Nota " << i+1 << ": ";
             cin >> N[i];
         }  
+        //utiliza media com 2 argumentos
         m = media(N[0], N[1]);
 
     }else{
-        cout << "Escolha a quantidade de notas que serao utilizadas, opcoes: 3 ou 4" << endl;
-        cin >> op; 
 
+        cout << "Escolha a quantidade de notas que serao utilizadas, opcoes: 3 ou 4" << endl;
         do{
+            cin >> op; 
             if(op == 3){
 
                 for(int i=0; i < op; i++){
                     cout << "Nota " << i+1 << ": ";
                     cin >> N[i];
                 }
-                
+                //utiliza media com 3 argumentos
                 m = media(N[0], N[1], N[2]);
 
             }else if(op == 4){
@@ -42,14 +44,21 @@ int main(){
                     cout << "Nota " << i+1 << ": ";
                     cin >> N[i];
                 }
-
+                //utiliza media com um vetor como argumento
                 m = media(N);
+            }else{
+                cout << "Opcao invalida -> opcoes validas: 3 ou 4" << endl;
             }
-        }while(op!=3 && op!=4);
+        }while(op!=3 && op!=4); //enquanto n for uma opcao valida
 
     }
 
+    //saida do console
     cout << "A media final: " << m << endl;
+    cout << "Conceito: ";
+    if(m >= 0  && m < 4){cout << "Reprovado." << endl;}
+    if(m >= 4  && m < 7){cout << "Prova final." << endl;}
+    if(m >= 7){cout << "Aprovado." << endl;}
 
     return 0;
 }
@@ -64,6 +73,6 @@ float media(float n1, float n2, float n3){
 
 float media(float n[4]){
     //melhor fazer um vetor
-    // tenho q fazer um sort crescente para usar media3
+    //tenho q fazer um sort crescente para usar media3
     return media(n[0],n[1],n[2]);
 }
