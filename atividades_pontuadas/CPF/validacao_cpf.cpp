7567123855bf7aea23094f3820484f5162af613a
Vitor França <vitor.franca@cear.ufpb.br>
2022-05-08 17:23:23 -0300

    validando cpfs de arq

--- atividades_pontuadas/CPF/validacao_cpf.cpp
+++ atividades_pontuadas/CPF/validacao_cpf.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
+//void CPF_get(string);
 void CPF_str2int(string , int []);
 void showArray(int [], int);
 int CPF_validation(int []);
 
 int main(){ 
-    string scpf;
-    int icpf[11], r;
+    const int N = 10; //quantidade de CPFs no arquivo     
+    string arc_name; //nome do arq que estão os CPFs 
+    string scpf; //cpf em formato string
+    int icpf[11]; //cpf como um array de inteiros
+    int valid; //flag de validação do CPF
+    ifstream input; //canal para arq de entrada
+    ofstream output; //canal para arq de saída
 
-    getline(cin, scpf);    
-    CPF_str2int(scpf, icpf);
-    r = CPF_validation(icpf);
-    
-    if(r){
-        cout << "CPF válido" << endl;
-    }else{
-        cout << "CPF inválido" << endl;
+    //abre o arquivo a ser lido
+    do{
+        cout << "Digite o nome do arquivo que estão os CPF a serem validados" << endl;
+        cout << "Ex: teste.txt" << endl;
+        getline(cin, arc_name);
+        input.open(arc_name);
+        if(!input){
+            cout << "\nO arquivo não pode ser aberto ou é inexistente." << endl;
+            cout << "-----Tente novamente.-----\n" << endl;
+        }
+    }while(!input);
+
+    cout << "\n---O arquivo foi aberto corretamente.---\n" << endl;
+
+    //cria o arquivo de saída
+    output.open("cpfs_validos.txt");    
+    if(!output){
+        cout << "Algum problema ocorreu durante a criação do arquivo de saída" << endl;
+        return 1;    
+    }
+     
+    for(int i=0; i<N; i++){
+        //pega o CPF do arquivo
+        getline(input, scpf); 
+        //verifica se o CPF é válido 
+        CPF_str2int(scpf, icpf);
+        //Adiciona ao array de flags 
+        valid = CPF_validation(icpf);
+        if(valid){
+            cout << "O CPF da linha " << i+1 << " é válido" << endl;
+            output << scpf << endl;
+        }else{
+            cout << "O CPF da linha " << i+1 << " é inválido" << endl;
+        }  
     }
     
-    
+    cout << "\n---O arquivo 'cpfs_validos.txt' foi criado corretamente---\n" << endl;
+    input.close();
+    output.close();
     return 0;
 }
 
