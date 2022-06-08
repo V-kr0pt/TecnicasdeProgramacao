c59ba7b0eef831caa840633e2014f96d776a34ce
kr0pt <vitor.franca@cear.ufpb.br>
2022-06-08 15:56:46 -0300

    adição do teste da sobrecarga do operador ++ na classe Cone

--- atividades_pontuadas/sobrecarga_de_operadores/main.cpp
+++ atividades_pontuadas/sobrecarga_de_operadores/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "class_circulo.h"
 #include "class_elipse.h"
+#include "class_cone.h"
 
 int main(){
     
     // ---- Teste da sobrecarga do operador + na classe Circulo ----
-    cout << "\n\t\t --- Sobrecarga do operador '+' na classe Circulo ---" << endl;
+    cout << "\n\t\t --- Sobrecarga do operador '+' na classe Circulo ---";
 
     Circulo circulo0, circulo1, circulo2;
     
@@ -23,7 +24,7 @@ int main(){
 
     // ---- Teste da sobrecarga do operador * na classe Elipse ----
 
-    cout << "\n\t\t--- Sobrecarga do operador '*' na classe Elipse ---" << endl;
+    cout << "\n\t\t--- Sobrecarga do operador '*' na classe Elipse ---";
 
     Elipse elipse0, elipse1, elipse2;
     
@@ -43,6 +44,21 @@ int main(){
     elipse2 = elipse0 * elipse1;
     cout << "r3 = r1 * r2 = " << elipse2.ratio() << endl; //valor esperado: r3 = 6
     cout << "R3 = R1 * R2 = " << elipse2.R << endl; //valor esperado: R3 = 20
+
+    // ---- Teste da sobrecarga do operador ++ na classe Cone ----
+
+    cout << "\n\t\t--- Sobrecarga do operador '++' na classe Cone ---";
+    Cone cone;
+    
+    cone.set_height(5); //adicionando uma altura ao cone
+    cout << "\n--- Cone ---" << endl;
+    cout << "r = " << cone.ratio() << endl; //apresentando o raio no console
+    cout << "H = " << cone.height() << endl; //apresentando altura no console
+
+    ++cone; //utilizando a sobrecarga do operador ++
+    cout << "\n--- Novo Cone ---" << endl; 
+    cout << "r = " << cone.ratio() << endl; //apresentando o (mesmo) raio no console
+    cout << "H = H++ = " << cone.height() << endl; //apresentando nova altura no console
 
     return 0;
 }
